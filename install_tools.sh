83125b2bcbf78ca1c0de311192e9fe4b355139a2
HamadaTomoki <hamada.tomoki01@gmail.com>
2023-01-07 16:28:49 +0900

    :wrench: Update configuration files.

--- install_tools.sh
+++ install_tools.sh
@@ -1,10 +1,17 @@
 
-# elixir 
+# node
+asdf plugin add nodejs
+asdf install nodejs 14.0.0
+asdf global nodejs 14.0.0
+
+# erlang
 asdf plugin add erlang
-asdf plugin add elixir
 asdf install erlang 25.2
-asdf install elixir 1.14.2-otp-25
 asdf global erlang 25.2
+
+# elixir 
+asdf plugin add elixir
+asdf install elixir 1.14.2-otp-25
 asdf global elixir 1.14.2-otp-25
 
 # phoenix
