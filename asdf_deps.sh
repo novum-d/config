d1261fee7fdc988a44fe4469d48a14be59ca1e4e
hamadatomoki <hamada.tomoki01@gmail.com>
2023-02-06 10:23:48 +0900

    :building_construction: chore: udpate asdf deps

--- asdf_deps.sh
+++ asdf_deps.sh
@@ -1,18 +1,29 @@
+# utils
 
-# node
+## github-cli
+asdf plugin-add github-cli https://github.com/bartlomiejdanek/asdf-github-cli.git
+asdf install github-cli latest
+asdf global github-cli latest
+
+
+# package tools
+
+## node
 NODE_VERSION=18.13.0
 asdf plugin add nodejs
 asdf install nodejs $NODE_VERSION
 asdf global nodejs $NODE_VERSION
 
-# erlang
+# programming language
+
+## erlang
 ERLANG_VERSION=25.2
 asdf plugin add erlang
 asdf install erlang $ERLANG_VERSION
 asdf global install erlang $ERLANG_VERSION
 asdf global erlang 25.2
 
-# elixir 
+## elixir 
 ELIXIR_VERSION=1.14.2-otp-25
 asdf plugin add elixir
 asdf install elixir $ELIXIR_VERSION
@@ -21,3 +32,5 @@ asdf global elixir $ELIXIR_VERSION
 # phoenix
 mix local.hex --force
 mix archive.install hex phx_new --force
+
+
