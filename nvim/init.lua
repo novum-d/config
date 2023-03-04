b71f8dd81bf2b5323c5fde6d419932801f53ab91
hamadatomoki <hamada.tomoki01@gmail.com>
2023-03-04 12:03:12 +0900

    :building_construction: chore: add phoenix snippet with luasnip

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,14 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 vim.api.nvim_set_keymap("i", "jj", "<esc>", { noremap = true, silent = true })
+
+require("luasnip.loaders.from_vscode").lazy_load({ paths = "./lua/snip/snippets/" })
+
+-- local cmp = require("cmp")
+-- cmp.setup({
+--   sources = cmp.config.sources({
+--     { name = "nvim_lsp" },
+--   }, {
+--     { name = "buffer" },
+--   }),
+-- })
---
+++ nvim/lua/plugins/cmp.lua
@@ -0,0 +1,7 @@
+return {
+  { "L3MON4D3/LuaSnip" },
+  { "hrsh7th/nvim-cmp" },
+  { "hrsh7th/cmp-nvim-lsp" },
+  { "saadparwaiz1/cmp_luasnip" },
+  { "rafamadriz/friendly-snippets" },
+}
