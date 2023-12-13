5c13e6a4ec5791077a6b32ed7979fd0a0d4af096
novumd <hamada.tomoki@email.com>
2023-12-14 01:00:42 +0900

    c

--- flake.nix
+++ flake.nix
@@ -1,22 +1,31 @@
 {
+  description = "NixOS configuration of novumd";
+
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs: {
-    homeConfigurations = {
-      "novumd" = inputs.home-manager.lib.homeManagerConfiguration {
-        system = "with x86_64-linux";
-        homeDirectory = "/home/novumd";
-        username = "novumd";
-        configuration.imports = [ ./home.nix ];
+  outputs = inputs@{ nixpkgs, home-manager, ... }: {
+    formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixpkgs-fmt;
+    nixosConfigurations = {
+      nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.novumd = import ./home/linux;
+
+            # Optionally, use home-manager.extraSpecialArgs to pass
+            # arguments to home.nix
+          }
+        ];
       };
     };
   };
-
 }
 
