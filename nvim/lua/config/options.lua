8967f170b0e8aeb3f9292f5820d96195543a6d5b
novum-d <hamada.tomoki01@gmail.com>
2025-01-06 03:01:35 +0900

    Update configuration

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+vim.g.lazyvim_rust_diagnostics = "rust-analyzer"
+vim.g.rustfmt_autosave = 1
+
+vim.api.nvim_set_keymap("i", "jj", "<esc>", { noremap = true, silent = true })
