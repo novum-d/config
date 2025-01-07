6bb8618d34d61e9fdbae370852c4424d4207e3f6
TomokiHamada <t.hamada@fenrir.co.jp>
2025-01-07 19:03:33 +0900

    Update configuration

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,6 +2,6 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.g.lazyvim_rust_diagnostics = "rust-analyzer"
-vim.g.rustfmt_autosave = 1
+-- vim.g.rustfmt_autosave = 1
 
 vim.api.nvim_set_keymap("i", "jj", "<esc>", { noremap = true, silent = true })
