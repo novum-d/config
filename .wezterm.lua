763b91f23533d10270804d0fd59915d81e705e7a
novum-d <hamada.tomoki01@gmail.com>
2025-11-15 16:38:53 +0900

    🏗️ chore:  update config

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,16 +1,12 @@
 local wezterm = require("wezterm")
+local config = {}
 
-local config = {
-	color_scheme = "s3r0 modified (terminal.sexy)",
-	font = wezterm.font_with_fallback({
-		{ family = "Cica" },
-		{ family = "JetBrains Nerd Mono" },
-		{ family = "Symbols Nerd Font Mono" },
-	}),
-	font_size = 18,
-	window_background_opacity = 0.85,
-	use_ime = true,
-	macos_window_background_blur = 20,
-}
+if wezterm.config_builder then
+	config = wezterm.config_builder()
+end
+
+config.color_scheme = "s3r0 modified (terminal.sexy)"
+config.warn_about_missing_glyphs = false
+config.font_size = 14.0
 
 return config
--- home.nix
+++ home.nix
@@ -1,7 +1,124 @@
-{ pkgs, ... }: {
-   programs.home-manager.enable = true;
+{ config, pkgs, ... }:
 
-   home.packages = [
-    pkgs.cowsay
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      # ./programs/wezterm
+      ./programs/git
+    ];
+
+  # TODO please change the username & home direcotry to your own
+  home.username = "novumd";
+  home.homeDirectory = "/home/novumd";
+
+  # link the configuration file in current directory to the specified location in home directory
+  # home.file.".config/i3/wallpaper.jpg".source = ./wallpaper.jpg;
+
+  # link all files in `./scripts` to `~/.config/i3/scripts`
+  # home.file.".config/i3/scripts" = {
+  #   source = ./scripts;
+  #   recursive = true;   # link recursively
+  #   executable = true;  # make all files executable
+  # };
+
+  # encode the file content in nix configuration file directly
+  # home.file.".xxx".text = ''
+  #     xxx
+  # '';
+
+  # set cursor size and dpi for 4k monitor
+  xresources.properties = {
+    "Xcursor.size" = 16;
+    "Xft.dpi" = 172;
+  };
+
+  # Packages that should be installed to the user profile.
+  home.packages = with pkgs; [
+    # here is some command line tools I use frequently
+    # feel free to add your own or remove some of them
+    
+    # proggramming langage
+    erlang
+    elixir
+    nodePackages.pnpm
+    nodejs
+    rustup
+
+    # archives
+    zip
+    unzip
+    xz
+
+    # utils
+    ripgrep # recursively searches directories for a regex pattern
+    fzf # A command-line fuzzy finder
+    zoxide
+    zellij
+    lazygit
+    charles
+    xdg-user-dirs # LANG=C xdg-user-dirs --force
+    xclip
+    fd
+    gh
+    ghq
+    xdotool
+    tre-command
+    tree-sitter
+    vscode
+    httpie
+    neovim
+    #neovim-nightly
+    # androidStudioPackages.canary
+    
+    # gui apps
+    # albert
+    brave
+    anki
+    android-studio
+    jetbrains.rust-rover
+    jetbrains.webstorm
+    koodo-reader
+
+    gnome.gnome-tweaks
+    typora
+    discord
+    slack
+    zoom-us
+    
+    # system tools
+    pciutils # lspci
+    usbutils # lsusb
+    libinput # xwayland
+    evtest # confirm keybord input event
+    clang # wasm
+    python3
+    ffmpeg
+    graphviz # plant uml
   ];
+
+  # This value determines the home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update home Manager without changing this value. See
+  # the home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "23.11";
+
+
+  # Let home Manager install and manage itself.
+  programs.home-manager.enable = true;
+  # programs.neovim.enable = true;
+  programs.fish = {
+    enable = true;
+    interactiveShellInit = ''
+      . $HOME/repos/config/home-manager/programs/fish/config.fish;
+    '';
+  };
+  programs.java.enable = true;
+
+  programs = {
+    google-chrome.enable = true;
+  };
 }
