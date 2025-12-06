3c0669649436df83c3806b35b4cdf2c83d42e952
TomokiHamada <t.hamada@fenrir.co.jp>
2025-12-06 18:12:01 +0900

    🏗️ chore:  update config

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -5,3 +5,14 @@ vim.g.lazyvim_rust_diagnostics = "rust-analyzer"
 -- vim.g.rustfmt_autosave = 1
 
 vim.api.nvim_set_keymap("i", "jj", "<esc>", { noremap = true, silent = true })
+
+vim.opt.spelllang = { "en", "cjk" }
+
+vim.api.nvim_create_autocmd("ColorScheme", {
+  callback = function()
+    vim.api.nvim_set_hl(0, "SpellBad", { fg = "#669966", undercurl = true })
+    vim.api.nvim_set_hl(0, "SpellCap", { fg = "#669966", undercurl = true })
+    vim.api.nvim_set_hl(0, "SpellRare", { fg = "#669966", undercurl = true })
+    vim.api.nvim_set_hl(0, "SpellLocal", { fg = "#669966", undercurl = true })
+  end,
+})
